e0308a33b228a9b9b8503a8228cad30cf3bf20c6
pyro <EvilPyro@protonmail.com>
2022-01-26 01:09:14 +0100

    cleanup

--- Deploy/translation_worker/run.sh
+++ Deploy/translation_worker/run.sh
@@ -3,4 +3,5 @@
 export PYTHONUNBUFFERED=1
 cd "$(dirname "$0")"
 celery -A tasks worker -P solo -Q translation_high --detach --loglevel=WARNING
+celery -A tasks worker -P solo -Q translation_high --detach --loglevel=WARNING
 celery -A tasks worker -P solo -Q translation_low,translation_high --loglevel=WARNING
